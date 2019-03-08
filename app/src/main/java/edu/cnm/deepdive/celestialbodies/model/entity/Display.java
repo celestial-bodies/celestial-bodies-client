9320c343ede111fa62267014a191d7d1335f4eb5
Todd A Nordquist <toddnordquist144@gmail.com>
2019-03-08 14:58:06 -0700

    Added front end entity classes.

---
+++ app/src/main/java/edu/cnm/deepdive/celestialbodies/model/entity/Display.java
@@ -0,0 +1,107 @@
+package edu.cnm.deepdive.celestialbodies.model.entity;
+
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import java.util.Date;
+
+
+@Entity(
+    foreignKeys = @ForeignKey(
+        entity = User.class,
+        parentColumns = "user_id", childColumns = "user_id",
+        onDelete = ForeignKey.CASCADE
+    )
+)
+
+public class Display {
+
+  @ColumnInfo(name = "display_id", index = true)
+  private long displayId;
+
+  @ColumnInfo(name = "user_id", index = true)
+  private long userId;
+
+  @ColumnInfo(name = "date", index = true)
+  private Date date;
+
+  @ColumnInfo(name = "latitude", index = true)
+  private float latitude;
+
+  @ColumnInfo(name = "longitude", index = true)
+  private float longitude;
+
+  @ColumnInfo(name = "ascension", index = true)
+  private float ascension;
+
+  @ColumnInfo(name = "declination", index = true)
+  private float declination;
+
+  @ColumnInfo(name = "log", index = true)
+  private String log;
+
+  public long getDisplayId() {
+    return displayId;
+  }
+
+  public void setDisplayId(long displayId) {
+    this.displayId = displayId;
+  }
+
+  public long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public float getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(float latitude) {
+    this.latitude = latitude;
+  }
+
+  public float getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(float longitude) {
+    this.longitude = longitude;
+  }
+
+  public float getAscension() {
+    return ascension;
+  }
+
+  public void setAscension(float ascension) {
+    this.ascension = ascension;
+  }
+
+  public float getDeclination() {
+    return declination;
+  }
+
+  public void setDeclination(float declination) {
+    this.declination = declination;
+  }
+
+  public String getLog() {
+    return log;
+  }
+
+  public void setLog(String log) {
+    this.log = log;
+  }
+}
---
+++ app/src/main/java/edu/cnm/deepdive/celestialbodies/model/entity/User.java
@@ -0,0 +1,88 @@
+package edu.cnm.deepdive.celestialbodies.model.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(indices = @Index(value = "date", unique = true))
+
+public class User {
+
+
+  @ColumnInfo(name = "user_id")
+  @PrimaryKey(autoGenerate = true)
+  private long userId;
+
+  @ColumnInfo(name = "name", index = true)
+  private String name;
+
+  @ColumnInfo(name = "favorites",index = true)
+  private String favorites;
+
+  @ColumnInfo(name = "city", index = true)
+  private String city;
+
+  @ColumnInfo(name = "zipe_code", index = true)
+  private long zipCode;
+
+  @ColumnInfo(name = "email", index = true)
+  private String email;
+
+  public long getId() {
+    return userId;
+  }
+
+  public void setId(long id) {
+    this.userId = id;
+  }
+
+  public long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFavorites() {
+    return favorites;
+  }
+
+  public void setFavorites(String favorites) {
+    this.favorites = favorites;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public long getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(long zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+}
