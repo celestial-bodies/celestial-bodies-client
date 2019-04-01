6374cab03b991c3bc9c4f225aeaa97e20b442fb5
McSmallpox <ryanwands18@gmail.com>
2019-04-01 16:59:23 -0600

    updated DAOs

--- app/src/main/java/edu/cnm/deepdive/celestialbodies/model/dao/StarDisplayDao.java
+++ app/src/main/java/edu/cnm/deepdive/celestialbodies/model/dao/StarDisplayDao.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.celestialbodies.model.dao;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import edu.cnm.deepdive.celestialbodies.model.entity.StarDisplay;
@@ -11,6 +12,9 @@ public interface StarDisplayDao {
 
   @Insert
   List<StarDisplay> insert(StarDisplay... displays);
+
+  @Delete
+  List<StarDisplay> delete(StarDisplay...displays);
 
   @Query("SELECT * FROM StarDisplay")
   List<StarDisplay> findAll();
--- app/src/main/java/edu/cnm/deepdive/celestialbodies/model/dao/UserDao.java
+++ app/src/main/java/edu/cnm/deepdive/celestialbodies/model/dao/UserDao.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.celestialbodies.model.dao;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import edu.cnm.deepdive.celestialbodies.model.entity.User;
@@ -13,8 +14,12 @@ public interface UserDao {
   @Insert
   List<User> insert(User... users);
 
+  @Delete
+  List<User> delete(User...users);
+
   @Query("SELECT * FROM User")
   public List<User> findAll();
+
 
 
 }
