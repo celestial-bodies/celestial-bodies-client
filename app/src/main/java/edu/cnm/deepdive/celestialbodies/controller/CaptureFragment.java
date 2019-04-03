d66adf8b0703fbeb3f5f796929e290bfd835b260
Todd A Nordquist <toddnordquist144@gmail.com>
2019-04-03 16:43:57 -0600

    Added CaptureFragment and added imageview and button to fragment_capture. Added DisplayWebService for querying star data and image from wikisky.

--- app/src/main/java/edu/cnm/deepdive/celestialbodies/controller/CaptureFragment.java
+++ app/src/main/java/edu/cnm/deepdive/celestialbodies/controller/CaptureFragment.java
@@ -1,15 +1,19 @@
 package edu.cnm.deepdive.celestialbodies.controller;
 
-import android.net.Uri;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 import edu.cnm.deepdive.celestialbodies.R;
+import edu.cnm.deepdive.celestialbodies.service.DisplayWebService.GetImageFromWikiSkyTask;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass. Activities that contain this fragment must implement the
@@ -19,24 +23,12 @@ import edu.cnm.deepdive.celestialbodies.R;
  */
 public class CaptureFragment extends Fragment {
 
-
-
-  private OnFragmentInteractionListener mListener;
+  private ImageView wikiImage;
+  Button captureButton;
 
   public CaptureFragment() {
     // Required empty public constructor
   }
-
-
-
-//  @Override
-//  public void onCreate(Bundle savedInstanceState) {
-//    super.onCreate(savedInstanceState);
-//    if (getArguments() != null) {
-//      mParam1 = getArguments().getString(ARG_PARAM1);
-//      mParam2 = getArguments().getString(ARG_PARAM2);
-//    }
-//  }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,35 +37,31 @@ public class CaptureFragment extends Fragment {
     Toast.makeText(getActivity(), "Captured Screen", Toast.LENGTH_SHORT).show();
 
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_capture, container, false);
+    View view = inflater
+        .inflate(R.layout.fragment_capture, container, false);
+    wikiImage = view.findViewById(R.id.wiki_image);
+
+    captureButton = view.findViewById(R.id.bn_capture);
+    captureButton.setOnClickListener(v -> {
+
+      Map<String,String> imageMap = new HashMap<>();
+      imageMap.put("ra", "30");
+      imageMap.put("de", "25");
+      imageMap.put("max_stars", "50");
+
+      new GetImageTask().execute(imageMap);
+
+    });
+
+    return view;
   }
 
-  // TODO: Rename method, update argument and hook method into UI event
-  public void onButtonPressed(Uri uri) {
-    if (mListener != null) {
-      mListener.onFragmentInteraction(uri);
+  class GetImageTask extends GetImageFromWikiSkyTask {
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+      wikiImage.setImageBitmap(bitmap);
+
     }
-  }
-
-
-
-  @Override
-  public void onDetach() {
-    super.onDetach();
-    mListener = null;
-  }
-
-  /**
-   * This interface must be implemented by activities that contain this fragment to allow an
-   * interaction in this fragment to be communicated to the activity and potentially other fragments
-   * contained in that activity.
-   * <p>
-   * See the Android Training lesson <a href= "http://developer.android.com/training/basics/fragments/communicating.html"
-   * >Communicating with Other Fragments</a> for more information.
-   */
-  public interface OnFragmentInteractionListener {
-
-    // TODO: Update argument type and name
-    void onFragmentInteraction(Uri uri);
   }
 }
